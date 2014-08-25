581cdab515d6ca1ef251bc01e60295dfce353213
Holger Janßen <post@holgerjanssen.de>
2014-08-25 22:15:16 +0200

    corrected naming of operations (in translations)

--- modules/hoja_team_player/languages/de/tl_hoja_team.php
+++ modules/hoja_team_player/languages/de/tl_hoja_team.php
@@ -1,3 +1,3 @@
 <?php
 
-	$GLOBALS['TL_LANG']['tl_hoja_team']['edit_players'] = array ( "Spieler bearbeiten" );
+	$GLOBALS['TL_LANG']['tl_hoja_team']['edit_players'] = array ("Spieler bearbeiten", "Spieler bearbeiten");
--- modules/hoja_team_player/languages/en/tl_hoja_team.php
+++ modules/hoja_team_player/languages/en/tl_hoja_team.php
@@ -1,3 +1,3 @@
 <?php
 
-	$GLOBALS['TL_LANG']['tl_hoja_team']['edit_players'] = array ( "edit players" );
+	$GLOBALS['TL_LANG']['tl_hoja_team']['edit_players'] = array ( "edit players", "edit players" );
